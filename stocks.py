fd9e56e4c42808c0f612370904bd4bcf6dc2fa47
Diederick de Buck <diederick.de.buck@gmail.com>
2019-07-03 15:28:00 +0200

    dddd

--- stocks.py
+++ stocks.py
@@ -1,7 +1,11 @@
 import yfinance as yf
 
+REGULAR_MARKET_VOLUME = u'regularMarketVolume'
+
 stocks = ["SSYS", "DDD", "AAPL", "DASTY"]
 for stock in stocks:
     quote = yf.Ticker(stock)
-    print(quote.info)
+    quote_dict = quote.info
+    print(quote_dict[REGULAR_MARKET_VOLUME])
+
 
