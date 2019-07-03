be1cf53e5edbe822c965b3a7d3fe69c3ec04c6d6
Diederick de Buck <diederick.de.buck@gmail.com>
2019-07-03 16:36:30 +0200

    added sql

--- stocks.py
+++ stocks.py
@@ -15,7 +15,7 @@ mysql_handle = mysql.connector.connect(
                                passwd="j0sepace",
                                database="stockpit"
                                )
-def update_stock(quote_dict):
+def update_stock(quote_dict, mysql_handle):
     print(quote_dict[SYMBOL]
           , quote_dict[LONG_NAME]
           , quote_dict[REGULAR_MARKET_VOLUME]
@@ -24,16 +24,21 @@ def update_stock(quote_dict):
           , quote_dict[SHARES_OUTSTANING]
           , quote_dict[MARKET_CAP]
           )
+    mysql_cursor = mysql_handle.cursor()
+    sql = "UPDATE nasdaq SET long_name = %s WHERE symbol = %s"
+    val = (quote_dict[LONG_NAME], quote_dict[SYMBOL])
+    mysql_cursor.execute(sql, val)
+    mysql_handle.commit()
     return
 
-def retrieve_stocks(stocks):
+def retrieve_stocks(stocks, mysql_handle):
     for stock in stocks:
         quote = yf.Ticker(stock)
         quote_dict = quote.info
-        update_stock(quote_dict)
+        update_stock(quote_dict, mysql_handle)
     return
 
 stocks = ["SSYS", "DDD", "AAPL", "DASTY"]
-retrieve_stocks(stocks)
+retrieve_stocks(stocks, mysql_handle)
 
 
