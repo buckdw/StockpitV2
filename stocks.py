3fe9d6195943506cfeb49c17dbaae69ae84f5404
Diederick de Buck <diederick.de.buck@gmail.com>
2019-07-03 15:55:00 +0200

    ddddd

--- stocks.py
+++ stocks.py
@@ -1,11 +1,36 @@
 import yfinance as yf
+import mysql.connector
 
 REGULAR_MARKET_VOLUME = u'regularMarketVolume'
+SYMBOL = u'symbol'
+LONG_NAME = u'longName'
+REGULAR_MARKET_OPEN = u'regularMarketOpen'
+AVERAGE_DAILY_VOLUME_10DAY = u'averageDailyVolume10Day'
+SHARES_OUTSTANING = u'sharesOutstanding'
+MARKET_CAP = u'marketCap'
+
+mysql_handle = mysql.connector.connect(
+                               host="localhost",
+                               user="root",
+                               passwd="j0sepace",
+                               database="stockpit"
+                               )
+def retrieve(stocks):
+    for stock in stocks:
+        quote = yf.Ticker(stock)
+        quote_dict = quote.info
+        print(quote_dict[SYMBOL]
+          , quote_dict[LONG_NAME]
+          , quote_dict[REGULAR_MARKET_VOLUME]
+          , quote_dict[AVERAGE_DAILY_VOLUME_10DAY]
+          , quote_dict[REGULAR_MARKET_OPEN]
+          , quote_dict[SHARES_OUTSTANING]
+          , quote_dict[MARKET_CAP]
+
+          )
+    return
 
 stocks = ["SSYS", "DDD", "AAPL", "DASTY"]
-for stock in stocks:
-    quote = yf.Ticker(stock)
-    quote_dict = quote.info
-    print(quote_dict[REGULAR_MARKET_VOLUME])
+retrieve(stocks)
 
 
