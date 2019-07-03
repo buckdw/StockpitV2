b515519d4f7777b39a0b7490a45c5486cf108f97
Diederick de Buck <diederick.de.buck@gmail.com>
2019-07-03 16:18:34 +0200

    dddd

--- stocks.py
+++ stocks.py
@@ -15,22 +15,25 @@ mysql_handle = mysql.connector.connect(
                                passwd="j0sepace",
                                database="stockpit"
                                )
-def retrieve(stocks):
-    for stock in stocks:
-        quote = yf.Ticker(stock)
-        quote_dict = quote.info
-        print(quote_dict[SYMBOL]
+def update_stock(quote_dict):
+    print(quote_dict[SYMBOL]
           , quote_dict[LONG_NAME]
           , quote_dict[REGULAR_MARKET_VOLUME]
           , quote_dict[AVERAGE_DAILY_VOLUME_10DAY]
           , quote_dict[REGULAR_MARKET_OPEN]
           , quote_dict[SHARES_OUTSTANING]
           , quote_dict[MARKET_CAP]
-
           )
     return
 
+def retrieve_stocks(stocks):
+    for stock in stocks:
+        quote = yf.Ticker(stock)
+        quote_dict = quote.info
+        update_stock(quote_dict)
+    return
+
 stocks = ["SSYS", "DDD", "AAPL", "DASTY"]
-retrieve(stocks)
+retrieve_stocks(stocks)
 
 
