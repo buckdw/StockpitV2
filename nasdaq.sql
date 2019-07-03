6025b89c9cda5a01cf3fdb68e0275fe3a1861a40
Diederick de Buck <diederick.de.buck@gmail.com>
2019-07-03 17:31:01 +0200

    dddd

--- nasdaq.sql
+++ nasdaq.sql
@@ -2,15 +2,19 @@ SET NAMES latin1;
 SET FOREIGN_KEY_CHECKS = 0;
 
 CREATE TABLE `nasdaq` (
-`long_name` varchar(255) NOT NULL DEFAULT '',
 `symbol` varchar(8) NOT NULL DEFAULT '',
-`open` float NOT NULL DEFAULT '0',
+`long_name` varchar(255) NOT NULL DEFAULT '',
+`regular_market_open` float NOT NULL DEFAULT '0',
 `close` float NOT NULL DEFAULT '0',
+`market_cap` float NOT NULL DEFAULT '0',
 `eps` float NOT NULL DEFAULT '0',
 PRIMARY KEY (`symbol`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-insert into `nasdaq` values('Apple Inc.','AAPL','0','0','0');
+insert into `nasdaq` values('AAPL','Apple Inc.','203.28','0','936688000000','0'),
+('SSYS','Stratasys Ltd.','27.36','0','1494160000','0'),
+('DASTY','Dassault SystËmes SE','164.628','0','43373200000','0'),
+('DDD','3D Systems Corporation','8.9','0','1051390000','0');
 
 SET FOREIGN_KEY_CHECKS = 1;
 
