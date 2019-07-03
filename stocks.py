25e058948b9e2c82253e18a884eed30085cefd20
Diederick de Buck <diederick.de.buck@gmail.com>
2019-07-03 19:51:27 +0200

    first version

--- stocks.py
+++ stocks.py
@@ -15,16 +15,75 @@ def initialize_sql():
     return mysql.connector.connect(host="localhost", user="root", passwd="j0sepace", database="stockpit")
 
 
+def insert_stock(quote_dict, mysql_handle):
+    mysql_cursor = mysql_handle.cursor()
+    sql = """
+        INSERT INTO nasdaq (
+            symbol,
+            long_name,
+            regular_market_open,
+            close,
+            market_cap,
+            eps,
+            forward_pe
+        )
+        VALUES (
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s,
+            %s
+        )
+        """
+    val = (quote_dict[SYMBOL]
+           , quote_dict[LONG_NAME]
+           , quote_dict[REGULAR_MARKET_OPEN]
+           , 0
+           , quote_dict[MARKET_CAP]
+           , 0
+           , quote_dict[FORWARD_PE]
+           )
+    mysql_cursor.execute(sql, val)
+    mysql_handle.commit()
+    return
+
+
+def upsert_stock(quote_dict, mysql_handle):
+    mysql_cursor = mysql_handle.cursor()
+    sql = """
+        SELECT *
+          FROM nasdaq
+         WHERE symbol = %s
+        """
+    val = (quote_dict[SYMBOL], )
+    mysql_cursor.execute(sql, val)
+    record = mysql_cursor.fetchone()
+    mysql_handle.commit()
+    if record is None:
+        insert_stock(quote_dict, mysql_handle)
+        return
+    update_stock(quote_dict, mysql_handle)
+    return
+
+
 def update_stock(quote_dict, mysql_handle):
     print(quote_dict[SYMBOL], quote_dict[LONG_NAME])
     mysql_cursor = mysql_handle.cursor()
-    sql = "UPDATE nasdaq SET long_name = %s, regular_market_open = %s, market_cap = %s, forward_pe = %s WHERE symbol = %s"
+    sql = """
+        UPDATE nasdaq
+           SET long_name = %s,
+               regular_market_open = %s,
+               market_cap = %s,
+               forward_pe = %s
+         WHERE symbol = %s
+        """
     val = (quote_dict[LONG_NAME]
            , quote_dict[REGULAR_MARKET_OPEN]
            , quote_dict[MARKET_CAP]
            , quote_dict[FORWARD_PE]
-           , quote_dict[SYMBOL]
-           )
+           , quote_dict[SYMBOL])
     mysql_cursor.execute(sql, val)
     mysql_handle.commit()
     return
@@ -34,7 +93,7 @@ def retrieve_stocks(stocks, mysql_handle):
     for stock in stocks:
         quote = yf.Ticker(stock)
         quote_dict = quote.info
-        update_stock(quote_dict, mysql_handle)
+        upsert_stock(quote_dict, mysql_handle)
     return
 
 
