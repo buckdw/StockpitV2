a9bd64f4f0b5bbf085dd03232d9b456f6688ab31
Diederick de Buck <diederick.de.buck@gmail.com>
2019-07-08 23:53:11 +0200

    dddd

--- stockinfo.py
+++ stockinfo.py
@@ -9,6 +9,29 @@ def initialize_sql():
                                    , passwd="j0sepace"
                                    , database="stockpit"
                                    )
+def retrieve_stocks(stocks, mysql_handle):
+    average_response_network = 0
+    average_response_sql = 0
+    stock_count = 0
+    for stock in stocks:
+        time_start = time.clock()
+        #        quote = yf.Ticker(stock)
+        time_delta_network = time.clock() - time_start
+        average_response_network = average_response_network + time_delta_network
+        stock_count = stock_count + 1
+        if quote:
+            quote_dict = validate_quote_dict(quote.info)
+            time_start = time.clock()
+            #            upsert_stock(quote_dict, mysql_handle)
+            time_delta_sql = time.clock() - time_start
+            average_response_sql = average_response_sql + time_delta_sql
+                # print("-------------------------------")
+                # print("Stocks={:d}".format(stock_count))
+                # print("Network response average={:4.2f}".format((average_response_network * 1000 * 1000) / stock_count))
+                # print("SQL response average={:4.2f}".format((average_response_sql * 1000 * 1000) / stock_count))
+                # print("-------------------------------")
+    return
+
 
 def load_stocks(stock_filename):
     stocks = list()
@@ -27,3 +50,4 @@ if __name__ == "__main__":
 
     stocks = load_stocks(args.file)
     mysql_handle = initialize_sql()
+    retrieve_stocks(stocks, mysql_handle)
