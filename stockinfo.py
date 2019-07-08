c937cdab12493ab4b31360de2d8a9b46b860a2c3
Diederick de Buck <diederick.de.buck@gmail.com>
2019-07-08 23:44:05 +0200

    ddd

--- stockinfo.py
+++ stockinfo.py
@@ -4,3 +4,13 @@ import json
 import time
 
 from yahoo_fin.stock_info import *
+
+
+#
+#   main code
+#
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, required=True)
+    args = parser.parse_args()
+
