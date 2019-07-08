d8eaad1d289f1908c96b0020e6bdacb53a4183fe
Diederick de Buck <diederick.de.buck@gmail.com>
2019-07-08 23:45:33 +0200

    dddd

--- stockinfo.py
+++ stockinfo.py
@@ -5,6 +5,19 @@ import time
 
 from yahoo_fin.stock_info import *
 
+def initialize_sql():
+    return mysql.connector.connect(host="localhost"
+                                   , user="root"
+                                   , passwd="j0sepace"
+                                   , database="stockpit"
+                                   )
+
+def load_stocks(stock_filename):
+    stocks = list()
+    with open(stock_filename) as stock_file:
+        for line in stock_file:
+            stocks.append(line)
+    return stocks
 
 #
 #   main code
@@ -14,3 +27,5 @@ if __name__ == "__main__":
     parser.add_argument('--file', type=str, required=True)
     args = parser.parse_args()
 
+    stocks = load_stocks(args.file)
+    mysql_handle = initialize_sql()
